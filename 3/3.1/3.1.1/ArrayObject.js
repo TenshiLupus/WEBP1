ef18a36099797f42c8545a0ea570437658ebaca3
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-07-06 15:51:58 +0200

    3.1.1

--- 3/3.1/3.1.1/ArrayObject.js
+++ 3/3.1/3.1.1/ArrayObject.js
@@ -1,56 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
 
-    var numbers = new Array();
-    var controller = new Date();
-    var regex = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
-    var fullDate = '';
+const numbers = new Array();
+const controller = new Date();
+const regex = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
+const fullDate = new String("");
+const condition = new Boolean(true);
 
-    numbers[2] = controller.getFullYear()
-    numbers[1] = controller.getMonth()
-    numbers[0] = controller.getDay()
+// allocate each date segment to a respective index
+numbers[2] = controller.getFullYear()
+numbers[1] = controller.getMonth()
+numbers[0] = controller.getDay()
 
-    for (var i = 0; i < numbers.length; i++) {
-        fullDate += numbers[i];
-        if (i != numbers.length - 1) {
-            fullDate += '-'
-        }
+// Concatenate numbers to a single string
+for (let i = 0; i < numbers.length; i++) {
+    fullDate += numbers[i];
+    if (i != numbers.length - 1) {
+        fullDate += '-'
     }
+}
 
 
 
-    document.querySelector('form').onsubmit = e => {
-        e.preventDefault();
-        alert("entered")
-        const userInput = document.querySelector('#user-input').value
-        let result = regex.test(userInput)
-        console.log(result)
-        if (result) {
-            const li = document.createElement('li')
-            li.innerHTML = numbers.join("-")
-            document.querySelector('#data-list').append(li)
-            console.log(numbers)
-            console.log(fullDate)
-            
-            if (Math.round(Math.random())) {
-                let userDate = userInput.split("-")
-                for (var j = 0; j < userDate.length; j++) {
-                    userDate[j] = Number.parseInt(userDate[j])
-                }
-                userDate[0] = numbers[0]
-                userDate = userDate.join("-")
-                const secondLi = document.createElement('li')
-                secondLi.innerHTML = userDate
-                console.log(secondLi)
-                document.querySelector('#data-list').append(secondLi)
-                alert("block ran")
+document.querySelector('form').onsubmit = e => {
+    // Hijack normal loading behavior
+    e.preventDefault();
+    alert("entered")
+
+    // select the form element in the page.
+    const userInput = document.querySelector('#user-input').value
+    let result = regex.test(userInput)
+    console.log(result)
+    if (result) {
+        const li = document.createElement('li')
+        li.innerHTML = numbers.join("-")
+        document.querySelector('#data-list').append(li)
+        console.log(numbers)
+        console.log(fullDate)
+
+        if (Math.round(Math.random())) {
+            let userDate = userInput.split("-")
+            for (var j = 0; j < userDate.length; j++) {
+                userDate[j] = Number.parseInt(userDate[j])
             }
-            numbers = fullDate.split("-")
-            document.querySelector('#user-input').value = ''
+            userDate[0] = numbers[0]
+            userDate = userDate.join("-")
+            const secondLi = document.createElement('li')
+            secondLi.innerHTML = userDate
+            console.log(secondLi)
+            document.querySelector('#data-list').append(secondLi)
+            alert("block ran")
         }
-        return false
+        numbers = fullDate.split("-")
+        document.querySelector('#user-input').value = ''
     }
+    return false
+}
 
-
-})
 
 
